c204d17f7c985cc769e92ab092c479c572374dc8
Laumania <mads@laumania.net>
2015-11-21 14:48:19 +0100

    Added moveable horse

---
+++ src/Mokkel/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerMovement : MonoBehaviour 
+{
+	public float Speed = 50f;
+	private Rigidbody2D _body;
+
+	// Use this for initialization
+	void Start () 
+	{
+		_body = this.GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		var horizontalMovement = CrossPlatformInputManager.GetAxis("Horizontal");
+
+		if(horizontalMovement != 0f)
+			this.transform.localScale = new Vector3 (-horizontalMovement,1f,1f);
+
+		_body.AddForce (new Vector2 (horizontalMovement * Speed * Time.deltaTime, 0f), ForceMode2D.Impulse);
+	}
+}
