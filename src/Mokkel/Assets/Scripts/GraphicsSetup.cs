34a5f754fc06db04c381840b5e26eafcea508dca
Laumania <mads@laumania.net>
2015-11-21 15:06:48 +0100

    Added Bat to scene

---
+++ src/Mokkel/Assets/Scripts/GraphicsSetup.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GraphicsSetup : MonoBehaviour 
+{
+	void Awake()
+	{
+		//Make iOS run smooth :)
+		Application.targetFrameRate = 60;
+	}
+}
